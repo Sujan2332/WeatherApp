a9dbf326c8437038db696ffbb3d95402c9c86d98
Sujan2332 <saisujan.s03@gmail.com>
2024-12-26 19:04:38 +0530

    svg

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/Youtube-Reactjs/',
+  esbuild: {
+    jsxInject: 'import React from "react";',
+  },
 })
