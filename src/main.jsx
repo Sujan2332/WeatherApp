9d0dc070b1b7b9d4daae6f0da51c851c314ddc6e
Sujan2332 <saisujan.s03@gmail.com>
2024-12-26 19:14:48 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from '../src/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
