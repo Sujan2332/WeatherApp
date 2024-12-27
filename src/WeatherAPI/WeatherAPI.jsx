4d2aa192573ffa6f310135ad5c9bd809166f1a8f
Sujan2332 <saisujan.s03@gmail.com>
2024-12-27 14:06:14 +0530

    background

--- src/WeatherAPI/WeatherAPI.jsx
+++ src/WeatherAPI/WeatherAPI.jsx
@@ -12,7 +12,7 @@ const WeatherAPI = () => {
     const [city, setCity] = useState("");
     const [backgroundVideo, setBackgroundVideo] = useState(null);
     const apiKey = import.meta.env.VITE_API_KEY;
-
+    console.log("Background")
     const fetchApi = useCallback(async () => {
         if (city === "") {
             setBackgroundVideo("default");
